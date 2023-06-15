813839849592a47136e1dc82786529fbdd60b87d
Raimundo Botelho <raimundo.botelho@gmail.com>
2023-06-15 08:53:48 -0300

    initial commit

--- graalvm/release.sh
+++ graalvm/release.sh
@@ -1,36 +1,32 @@
 #!/bin/bash
 
 INFO="Installation\n\n
-1. After unzipping the release file, the codegen folder was created\n
-2. Copy the codegen folder to home directory ~/\n
-3. Add the codegen folder to the PATH environment variable\n
-4. Run the following command: codegen -v\n\n
-If everything has been done correctly, you will see the following output:\n\n
-Codegen command line interface (CLI)\n
-Version: 2.1.5\n"
+1. After unzipping the release file, the model-dsl-json folder was created\n
+2. Copy the model-dsl-json folder to home directory ~/\n
+3. Add the model-dsl-json folder to the PATH environment variable\n"
 
 # Linux
 cd output
-mkdir codegen
-cp codegen-linux codegen/codegen
-echo $INFO | tee codegen/installation.txt > /dev/null
-zip -r codegen-linux.zip codegen
-rm -Rf codegen
+mkdir model-dsl-json
+cp model-dsl-json-linux model-dsl-json/model-dsl-json
+echo $INFO | tee model-dsl-json/installation.txt > /dev/null
+zip -r model-dsl-json-linux.zip model-dsl-json
+rm -Rf model-dsl-json
 
 # Macos
 cd output
-mkdir codegen
-cp codegen-macos codegen/codegen
-echo $INFO | tee codegen/installation.txt > /dev/null
-zip -r codegen-macos.zip codegen
-rm -Rf codegens
+mkdir model-dsl-json
+cp model-dsl-json-macos model-dsl-json/model-dsl-json
+echo $INFO | tee model-dsl-json/installation.txt > /dev/null
+zip -r model-dsl-json-macos.zip model-dsl-json
+rm -Rf model-dsl-jsons
 
 # Window
 cd output
-mkdir codegen
-cp ../../target/codegen/* codegen
-zip -r codegen-windows.zip codegen
-rm -Rf codegen
+mkdir model-dsl-json
+cp ../../target/model-dsl-json/* model-dsl-json
+zip -r model-dsl-json-windows.zip model-dsl-json
+rm -Rf model-dsl-json
 
 # Finish
-rm -Rf codegen
+rm -Rf model-dsl-json
